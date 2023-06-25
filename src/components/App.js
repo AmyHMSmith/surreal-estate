41d22ffd7c6cafd5d476e767b82a2eeaad09d294
AmyHMSmith <amyhmsmith@gmail.com>
2023-06-25 20:08:53 +0100

    initial setup

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import '../styles/app.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      
+          <h2>Surreal Estate</h2>
+    
+    </div>
+  );
+}
+
+export default App;
